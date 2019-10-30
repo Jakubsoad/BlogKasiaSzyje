6132aa0530218aef4c61b357adff703e86c9cbc1
Jakub Mróz <jakub.soad@gmail.com>
2019-10-30 10:58:46 +0100

    responsive img for mobile

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -2,7 +2,7 @@
 <div id="promo">
         <div class="jumbotron shadow-lg">
             <h2>Nasi ludzie jeszcze nad tym pracują</h2>
-            <img alt="work in progress" src="https://media.giphy.com/media/l3nWpMnnbQjaXtP7W/giphy.gif">
+            <img class="img-fluid" alt="work in progress" src="https://media.giphy.com/media/l3nWpMnnbQjaXtP7W/giphy.gif">
         </div>
 </div>
 @include('layout.footer')
